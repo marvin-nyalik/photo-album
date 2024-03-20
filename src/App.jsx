eb95d6ea8bd6dce4773f87d0a99286d0ac2c62f3
MARVIN <o.silverygold@gmail.com>
2024-03-20 21:35:20 +0300

    Update boilerplate code

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import Typography from "@mui/material/Typography";
+import AppBar from '@mui/material/AppBar';
+import Container  from "@mui/material/Container";
+
+function App() {
+  return (
+    <>
+      <Container maxWidth="xl" sx={{ backgroundColor: "#324ea8"}}>
+        <Typography variant="h4">Photo Album</Typography>
+      </Container>
+    </>
+  )
+}
+
+export default App
+
