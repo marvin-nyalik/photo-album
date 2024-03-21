a24c7592b099ebee3bd5fe47a722b68349d46629
MARVIN <o.silverygold@gmail.com>
2024-03-21 21:34:22 +0300

    right navigation skeleton

---
+++ src/components/Rightbar.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import { Box } from '@mui/material'
+
+const Rightbar = () => {
+  return (
+    <Box 
+      flex={2}
+      sx={{ display:{ xs: "none", sm: "block"} }}
+    >
+      Rightbar
+    </Box>
+  )
+}
+
+export default Rightbar
