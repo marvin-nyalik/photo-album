f37e5d1a2bf433112661c6f1b333fc8e08bf1c8b
MARVIN <o.silverygold@gmail.com>
2024-03-23 21:28:37 +0300

    Add initial footer component

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -1,6 +1,14 @@
 import React from 'react'
 import Post from './Posts';
-import { Box } from '@mui/material';
+import { Avatar, Box, List, ListItem, ListItemButton, ListItemIcon, ListItemText, Typography, styled } from '@mui/material';
+import { Dashboard, Home, LocationCity, Phone } from '@mui/icons-material';
+
+const FooterBox = styled(Box)({
+  display: 'flex',
+  flexDirection: 'row',
+  alignItems: 'center',
+  gap: "10px"
+})
 
 const Feed = () => {
   return (
@@ -11,6 +19,45 @@ const Feed = () => {
       <Post/>
       <Post/>
       <Post/>
+      <FooterBox>
+        <Box flex={1} mt={5} mb={5}>
+          <Typography variant='p' fontWeight={200}>
+            <Avatar 
+            src="https://images.pexels.com/photos/3225517/pexels-photo-3225517.jpeg?auto=compress&cs=tinysrgb&w=600"></Avatar>
+            Alleagy Vegas Marvin
+          </Typography>
+          <Typography variant='h6' fontWeight={200}>
+            P.O Box 1335, Kisumu
+          </Typography>
+          <Typography variant='p' fontWeight={100}>
+            ceo@alleagy.co.kr
+          </Typography>
+        </Box>
+        <Box flex={1}>
+        <List>
+          <ListItem disablePadding>
+            <ListItemButton component="a" href="#">
+              <ListItemIcon>
+                <Phone />
+              </ListItemIcon>
+              <ListItemText primary="0743904791" />
+            </ListItemButton>
+          </ListItem>
+
+          <ListItem disablePadding>
+            <ListItemButton component="a" href="#">
+              <ListItemIcon>
+                <LocationCity />
+              </ListItemIcon>
+              <ListItemText primary="Location" />
+            </ListItemButton>
+          </ListItem>
+          </List>
+        </Box>
+        <Box flex={1}>
+          Real Madrid
+        </Box>
+      </FooterBox>
     </Box>
   )
 }
