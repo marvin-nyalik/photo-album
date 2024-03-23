447bdd9908789e20ece0e554803b41ea3a5d3bda
MARVIN <o.silverygold@gmail.com>
2024-03-23 21:29:04 +0300

    Create footer component

---
+++ src/components/Footer.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Box } from '@mui/material';
+
+const Footer = () => {
+  return (
+    <Box bgcolor="blue" sx={{ height: "30vh"}}>
+      Footer
+    </Box>
+  )
+}
+
+export default Footer
