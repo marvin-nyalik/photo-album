e9ab148d803c5adc9b3b0ca19530ba1c32845e1b
MARVIN <o.silverygold@gmail.com>
2024-03-23 21:28:49 +0300

    Update theme

--- src/App.jsx
+++ src/App.jsx
@@ -5,16 +5,17 @@ import Rightbar from "./components/Rightbar";
 import { Box, Stack, ThemeProvider, createTheme } from "@mui/material";
 import Add from "./components/Add";
 import { useState } from "react";
+import Footer from "./components/Footer";
 
 function App() {
   const [mode, setMode] = useState("light")
-  const darkTheme = createTheme({
+  const theme = createTheme({
     palette: {
       mode: mode
     }
   })
   return (
-    <ThemeProvider theme={darkTheme}>
+    <ThemeProvider theme={theme}>
     <Box bgcolor={"background.default"} color="text.primary">
       <Navbar/>
       <Stack direction="row">
